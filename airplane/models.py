bcf6053ee8c00bb631569a3d0942cccc187b1846
arturiermolenko <arturiermolenko@gmail.com>
2023-09-08 18:19:28 +0200

    Created models in apps airport, airplane, flight
    Created all models in apps airport, airplane, flight

--- airplane/models.py
+++ airplane/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class AirplaneType(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Airplane(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+    airplane_type = models.ForeignKey(AirplaneType, on_delete=models.CASCADE)
+
+    @property
+    def capacity(self):
+        return self.rows * self.seats_in_row
+
+    def __str__(self):
+        return f"{self.name}, capacity: {self.capacity}"
+
--- airport/models.py
+++ airport/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class City(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.country}: {self.name}"
+
+
+class Airport(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Route(models.Model):
+    source = models.ForeignKey(
+        Airport,
+        on_delete=models.CASCADE,
+        related_name="source"
+    )
+    destination = models.ForeignKey(
+        Airport,
+        on_delete=models.CASCADE,
+        related_name="destination"
+    )
+    distance = models.IntegerField()
+
+    def __str__(self):
+        return (f"{self.source}({self.source.city}) - "
+                f"{self.destination}({self.destination.city})")
