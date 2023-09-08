4bb5da7a451ad9435d23690d169c2655b391fe23
arturiermolenko <arturiermolenko@gmail.com>
2023-09-08 11:05:45 +0200

    Fixed small bags in user/urls.py
    Added schema urls for drf-spectacular

--- airport_API_service/urls.py
+++ airport_API_service/urls.py
@@ -20,7 +20,8 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import (
     SpectacularSwaggerView,
-    SpectacularRedocView
+    SpectacularRedocView,
+    SpectacularAPIView
 )
 
 urlpatterns = [
@@ -29,13 +30,14 @@ urlpatterns = [
     path("api/airplane/", include("airplane.urls", namespace="airplane")),
     path("api/flight", include("flight.urls", namespace="flight")),
     path("api/user/", include("user.urls", namespace="user")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
-        "api/doc/swagger",
+        "api/doc/swagger/",
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui"
     ),
     path(
-        "api/doc/redoc",
+        "api/doc/redoc/",
         SpectacularRedocView.as_view(url_name="schema"),
         name="redoc"
     ),
